3523790bdb03d57221902385bf1baf01c5132f63
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2021-03-24 00:06:39 +0100

    websocket server sample code

--- src/main/java/it/vitalegi/rpgboard/be/MainVerticle.java
+++ src/main/java/it/vitalegi/rpgboard/be/MainVerticle.java
@@ -1,18 +1,23 @@
 package it.vitalegi.rpgboard.be;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.MultiMap;
 import io.vertx.core.Promise;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.http.ServerWebSocket;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.ErrorHandler;
 
 public class MainVerticle extends AbstractVerticle {
 	Logger log = LoggerFactory.getLogger(MainVerticle.class);
@@ -61,7 +66,20 @@ public class MainVerticle extends AbstractVerticle {
 					.put("message", "Hello " + name + " connected from " + address));
 		});
 
+		Handler<ServerWebSocket> wsHandler = (context) -> {
+			context.writeTextMessage("ping");
+			context.textMessageHandler((msg) -> {
+				System.out.println("Server " + msg);
+
+				if ((new Random()).nextInt(100) == 0) {
+					context.close();
+				} else {
+					context.writeTextMessage("ping2");
+				}
+			});
+		};
 		vertx.createHttpServer().requestHandler(router)//
+				.webSocketHandler(wsHandler)//
 				.listen(Integer.parseInt(System.getenv("PORT")), "0.0.0.0")//
 				.onSuccess(server -> {
 					System.out.println("Start server on port " + server.actualPort());
