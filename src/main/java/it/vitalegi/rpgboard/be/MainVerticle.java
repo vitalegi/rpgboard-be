db3ee494905846b334d5e62c4d43972cac43afd8
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2021-05-11 20:44:15 +0200

    sample oauth2 code

--- src/main/java/it/vitalegi/rpgboard/be/MainVerticle.java
+++ src/main/java/it/vitalegi/rpgboard/be/MainVerticle.java
@@ -3,9 +3,11 @@ package it.vitalegi.rpgboard.be;
 import io.reactivex.Completable;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.eventbus.EventBus;
 import io.vertx.reactivex.core.http.ServerWebSocket;
+import io.vertx.reactivex.ext.auth.oauth2.OAuth2Auth;
 import io.vertx.reactivex.ext.auth.oauth2.providers.FacebookAuth;
 import io.vertx.reactivex.ext.web.Router;
 import io.vertx.reactivex.ext.web.handler.CorsHandler;
@@ -30,10 +32,42 @@ public class MainVerticle extends AbstractVerticle {
     router
         .route()
         .handler(
-            CorsHandler.create("http://localhost:88885")
+            CorsHandler.create("http://localhost:8080")
                 .allowedMethod(HttpMethod.GET)
                 .allowedMethod(HttpMethod.POST)
-                .allowedMethod(HttpMethod.OPTIONS)); // .allowedHeader("Content-Type"));
+                .allowedMethod(HttpMethod.OPTIONS)
+                .allowedHeader("Content-Type"));
+
+    /*OAuth2Auth oauth2 =
+        OAuth2Auth.create(
+            vertx,
+            new OAuth2Options()
+                .setFlow(OAuth2FlowType.AUTH_CODE)
+                .setClientID("yyyyyyyyyyyyyyyy")
+                .setClientSecret("xxxxxxxxxxxxxx")
+                .setSite("https://facebook.com/login")
+                .setTokenPath("/oauth/access_token")
+                .setAuthorizationPath("/oauth/authorize"));
+
+    // OAuth2Auth oauth2 = OAuth2Auth.create(vertx, opt);
+    OAuth2Auth auth =
+        FacebookAuth.create(vertx, "yyyyyyyyyyy", "xxxxxxxxxx");
+
+    String authorization_uri =
+        auth.authorizeURL(
+            new JsonObject()
+                .put("redirect_uri", "http://localhost:8080/callback")
+                .put("scope", "notifications")
+                .put("state", "aaaaaa"));
+
+    router
+        .get("/oauth2/authorization")
+        .handler(
+            ctx -> {
+              log.info("authorization");
+              ctx.response().putHeader("Location", authorization_uri).setStatusCode(302).end();
+            });
+    */
 
 
     EventBus eventBus = vertx.eventBus();
