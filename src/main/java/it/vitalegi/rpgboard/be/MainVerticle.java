7aa5067dfb78c63c3130d4d8e826dd11e31d641c
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2021-07-26 20:13:36 +0200

    working websocket sample

--- src/main/java/it/vitalegi/rpgboard/be/MainVerticle.java
+++ src/main/java/it/vitalegi/rpgboard/be/MainVerticle.java
@@ -3,18 +3,25 @@ package it.vitalegi.rpgboard.be;
 import io.reactivex.Completable;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.bridge.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSBridgeOptions;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.eventbus.EventBus;
-import io.vertx.reactivex.core.http.ServerWebSocket;
 import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
 import io.vertx.reactivex.ext.web.handler.CorsHandler;
+import io.vertx.reactivex.ext.web.handler.sockjs.SockJSHandler;
 import it.vitalegi.rpgboard.be.handler.AccountAddHandler;
 import it.vitalegi.rpgboard.be.handler.AccountFindAllHandler;
 import it.vitalegi.rpgboard.be.handler.AccountFindByIdHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainVerticle extends AbstractVerticle {
   Logger log = LoggerFactory.getLogger(MainVerticle.class);
@@ -33,65 +40,42 @@ public class MainVerticle extends AbstractVerticle {
                 .allowedMethod(HttpMethod.GET)
                 .allowedMethod(HttpMethod.POST)
                 .allowedMethod(HttpMethod.OPTIONS)
-                .allowedHeader("Content-Type"));
-
-    /*OAuth2Auth oauth2 =
-        OAuth2Auth.create(
-            vertx,
-            new OAuth2Options()
-                .setFlow(OAuth2FlowType.AUTH_CODE)
-                .setClientID("yyyyyyyyyyyyyyyy")
-                .setClientSecret("xxxxxxxxxxxxxx")
-                .setSite("https://facebook.com/login")
-                .setTokenPath("/oauth/access_token")
-                .setAuthorizationPath("/oauth/authorize"));
-
-    // OAuth2Auth oauth2 = OAuth2Auth.create(vertx, opt);
-    OAuth2Auth auth =
-        FacebookAuth.create(vertx, "yyyyyyyyyyy", "xxxxxxxxxx");
-
-    String authorization_uri =
-        auth.authorizeURL(
-            new JsonObject()
-                .put("redirect_uri", "http://localhost:8080/callback")
-                .put("scope", "notifications")
-                .put("state", "aaaaaa"));
-
-    router
-        .get("/oauth2/authorization")
-        .handler(
-            ctx -> {
-              log.info("authorization");
-              ctx.response().putHeader("Location", authorization_uri).setStatusCode(302).end();
-            });
-    */
+                .allowedHeader("Content-Type")
+                .allowedHeader("Access-Control-Request-Method")
+                .allowedHeader("Access-Control-Allow-Credentials")
+                .allowedHeader("Access-Control-Allow-Origin")
+                .allowedHeader("Access-Control-Allow-Headers")
+                .allowedHeader("x-requested-with")
+                .allowedHeader("origin")
+                .allowedHeader("accept")
+                .allowCredentials(true));
 
     EventBus eventBus = vertx.eventBus();
     router.get("/api/account").handler(new AccountFindByIdHandler(eventBus));
     router.post("/api/account").handler(new AccountAddHandler(eventBus));
     router.get("/api/accounts").handler(new AccountFindAllHandler(eventBus));
 
+    SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+    sockJSHandler.bridge(
+        new SockJSBridgeOptions()
+            .addInboundPermitted(new PermittedOptions().setAddressRegex("websocket.*"))
+            .addOutboundPermitted(new PermittedOptions().setAddressRegex("websocket.*")));
+
+    router.route("/eventbus/*").handler(sockJSHandler);
+    List<String> names = new ArrayList<>();
+    eventBus.consumer("websocket.add-name", event -> {
+      log.info("Received a msg {}", event.toString());
+      names.add("");
+
+      JsonArray jsonArr = new JsonArray();
+      names.forEach(jsonArr::add);
+      eventBus.publish("websocket.names", jsonArr);
+    });
+
     return vertx
         .createHttpServer()
         .requestHandler(router)
-        .webSocketHandler(configureWebSocket())
         .rxListen(Integer.parseInt(System.getenv("PORT")), "0.0.0.0")
         .ignoreElement();
-  }
-
-  private Handler<ServerWebSocket> configureWebSocket() {
-    return (context) -> {
-      context.writeTextMessage("ping");
-      context.textMessageHandler(
-          (msg) -> {
-            System.out.println("Server " + msg);
-
-            if ((new Random()).nextInt(100) == 0) {
-              context.close();
-            } else {
-              context.writeTextMessage("ping2");
-            }
-          });
-    };
   }
 }
