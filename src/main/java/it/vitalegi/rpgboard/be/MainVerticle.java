3f01d7b1001064184114b30324764a1592600646
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2021-07-29 13:43:06 +0200

    feature: externalized application properties

--- src/main/java/it/vitalegi/rpgboard/be/MainVerticle.java
+++ src/main/java/it/vitalegi/rpgboard/be/MainVerticle.java
@@ -1,7 +1,12 @@
 package it.vitalegi.rpgboard.be;
 
 import io.reactivex.Completable;
+import io.reactivex.Single;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.config.ConfigRetriever;
 import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.eventbus.EventBus;
 import io.vertx.reactivex.ext.web.Router;
@@ -13,49 +18,91 @@ import it.vitalegi.rpgboard.be.security.FirebaseJWTAuthenticationHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.stream.Collectors;
+
 public class MainVerticle extends AbstractVerticle {
   Logger log = LoggerFactory.getLogger(MainVerticle.class);
+  private ConfigRetrieverOptions options;
 
   @Override
   public Completable rxStart() {
     log.info("start");
 
-    vertx.deployVerticle(new AccountVerticle());
-    vertx.deployVerticle(new GameVerticle());
-    Router router = Router.router(vertx);
+    log.info("Setup properties");
+    ConfigStoreOptions commonFileStore =
+        new ConfigStoreOptions()
+            .setType("file")
+            .setFormat("json")
+            .setOptional(false)
+            .setConfig(new JsonObject().put("path", "config.json"));
 
-    router
-        .route()
-        .handler(
-            CorsHandler.create("http://localhost:8080")
-                .allowedMethod(HttpMethod.GET)
-                .allowedMethod(HttpMethod.POST)
-                .allowedMethod(HttpMethod.OPTIONS)
-                .allowedHeader("Content-Type")
-                .allowedHeader("Access-Control-Request-Method")
-                .allowedHeader("Access-Control-Allow-Credentials")
-                .allowedHeader("Access-Control-Allow-Origin")
-                .allowedHeader("Access-Control-Allow-Headers")
-                .allowedHeader("x-requested-with")
-                .allowedHeader("origin")
-                .allowedHeader("accept")
-                .allowCredentials(true));
+    ConfigStoreOptions fileStore =
+        new ConfigStoreOptions()
+            .setType("file")
+            .setFormat("json")
+            .setOptional(false)
+            .setConfig(new JsonObject().put("path", "config-" + System.getenv("ENV") + ".json"));
 
-    FirebaseJWTAuthenticationHandler.init();
-    router.route("/api/*").blockingHandler(new FirebaseJWTAuthenticationHandler(), false);
+    ConfigRetrieverOptions options =
+        new ConfigRetrieverOptions().addStore(commonFileStore).addStore(fileStore);
 
-    EventBus eventBus = vertx.eventBus();
+    Single<JsonObject> rxConfig = ConfigRetriever.create(vertx, options).rxGetConfig();
+    rxConfig.doOnError(
+        error -> {
+          log.error("Failed to load props", error);
+          System.exit(0);
+        });
+    return rxConfig.flatMapCompletable(
+        config -> {
+          log.info("Setup properties done");
+          log.info("values: {}", config.encodePrettily());
+          vertx.deployVerticle(new AccountVerticle());
+          vertx.deployVerticle(new GameVerticle());
+          Router router = Router.router(vertx);
 
-    router.get("/api/account").handler(new AccountFindByIdHandler(eventBus));
-    router.post("/api/account").handler(new AccountAddHandler(eventBus));
-    router.get("/api/accounts").handler(new AccountFindAllHandler(eventBus));
+          router.route().handler(corsHandler(config));
 
-    WebSocketConfig.init(vertx, router);
+          FirebaseJWTAuthenticationHandler.init();
+          router.route("/api/*").blockingHandler(new FirebaseJWTAuthenticationHandler(), false);
 
-    return vertx
-        .createHttpServer()
-        .requestHandler(router)
-        .rxListen(Integer.parseInt(System.getenv("PORT")), "0.0.0.0")
-        .ignoreElement();
+          EventBus eventBus = vertx.eventBus();
+
+          router.get("/api/account").handler(new AccountFindByIdHandler(eventBus));
+          router.post("/api/account").handler(new AccountAddHandler(eventBus));
+          router.get("/api/accounts").handler(new AccountFindAllHandler(eventBus));
+
+          WebSocketConfig.init(vertx, router);
+
+          Completable out =
+              vertx
+                  .createHttpServer()
+                  .requestHandler(router)
+                  .rxListen(Integer.parseInt(System.getenv("PORT")), "0.0.0.0")
+                  .ignoreElement();
+          return out;
+        });
+  }
+
+  protected CorsHandler corsHandler(JsonObject config) {
+    JsonObject sec = config.getJsonObject("security");
+    log.info("Apply CORS configuration: {}", sec.encodePrettily());
+    CorsHandler corsHandler = CorsHandler.create();
+
+    corsHandler.addOrigins(
+        sec.getJsonArray("cors").stream().map(Object::toString).collect(Collectors.toList()));
+
+    corsHandler.allowedMethods(
+        sec.getJsonArray("allowedMethods").stream()
+            .map(Object::toString)
+            .map(HttpMethod::valueOf)
+            .collect(Collectors.toSet()));
+
+    corsHandler.allowedHeaders(
+        sec.getJsonArray("allowedHeaders").stream()
+            .map(Object::toString)
+            .collect(Collectors.toSet()));
+
+    corsHandler.allowCredentials(sec.getBoolean("allowCredentials"));
+    return corsHandler;
   }
 }
